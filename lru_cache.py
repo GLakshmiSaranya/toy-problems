fced7a60c922bae1b50ca63abf438e31a7152410
GLakshmiSaranya <saranyagundapaneni@msitprogram.net>
2020-04-13 23:49:20 +0530

    commit lru-cache with test cases

--- lru_cache.py
+++ lru_cache.py
@@ -7,9 +7,24 @@ class LRUCache():
 
     def put(self, key, value):
         pass
+        if self.capacity <= len(self.cache):
+            prev_key = min(self.lru.keys(), key = lambda k: self.lru[k])
+            self.lru.pop(prev_key)
+            self.cache.pop(prev_key)
+
+        self.lru[key] = self.count
+        self.cache[key] = value
+        self.count += 1
 
     def get(self, key):
         pass
+        if key in self.cache:
+            self.lru[key] = self.count
+            self.count += 1
+            return self.cache[key]
+        else:
+            return -1
 
     def get_cache(self):
         pass
+        return self.cache
